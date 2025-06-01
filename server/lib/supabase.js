1c658642779aea8523b4cf516136291da2e724b1
Marc Antoni Mas <103957841+mtmarctoni@users.noreply.github.com>
2025-06-01 18:36:50 +0200

    Updated supabase.js

--- server/lib/supabase.js
+++ server/lib/supabase.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
 dotenv.config();
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 
 if (!supabaseUrl || !supabaseServiceKey) {
